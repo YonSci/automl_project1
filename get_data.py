0a0142dbbd22361fa5c89cf569c422321b4de485
Yonas <yonas.mersha14@gmail.com>
2025-04-11 15:06:42 +0300

    data prepared for local tracking

--- get_data.py
+++ get_data.py
@@ -8,9 +8,9 @@ import numpy as np
 if os.path.isfile("data/data.csv"):
     n = 1
 else:
-    n = 50
+    n = 10
 
 for i in range(0,n):    
-    X, y = make_regression(10000,n_features = 10)
+    X, y = make_regression(100,n_features = 10)
     df = pd.DataFrame(X)
     df.to_csv("data/data.csv",mode='a')
